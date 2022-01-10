94e60d43e02629cbbd595d75066d72de1b96db2d
AlmaDividida <david_626180497@hotmail.com>
2022-01-09 22:13:43 -0600

    Se agrego la funcionalidad de leer Archivos JSON, se agrego el boton Subir Archivo

--- src/app/shared/visualizador/visualizador.component.ts
+++ src/app/shared/visualizador/visualizador.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisualizadorComponent implements OnInit {
 
-  constructor() { }
+  file: any | null = null;
+  buttonDisabled: boolean = true;
+  jsonObj: any | null = null;
+
+  // Inject service 
+  constructor(){}
 
   ngOnInit(): void {
   }
 
+  // OnChange File
+  onChange( event:any ) {
+    var fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      this.file = event.target.files[0];
+      this.buttonDisabled = false;
+    } else {
+      this.buttonDisabled = true;
+    }
+  }
+  
+  // OnClick del boton "Subir Archivo" 
+  onLoad() {
+      var fileToLoad = this.file;
+      var fileReader = new FileReader();
+      fileReader.onload = function(fileLoadedEvent){
+          var textFromFileLoaded:any = fileLoadedEvent.target?.result;
+          var json = JSON.parse(textFromFileLoaded);
+          console.log(json)
+      };
+
+      fileReader.readAsText(fileToLoad, "UTF-8");
+    }
 }
