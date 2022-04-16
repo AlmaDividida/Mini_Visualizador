dbf2409c891213a8a3043958086f0950fb0aae07
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-04-15 20:55:52 -0500

    Cambios a voronoi.ts
    - Cambia BoxGeometry por BufferGeometry.
    - Agrega AutoRotating con OrbitControls.
    - Agrega comentarios.
    - Refactor.

--- src/app/models/voronoi.ts
+++ src/app/models/voronoi.ts
@@ -1,129 +1,188 @@
+// Interface que implementan todos los modelos
 import { InterfaceLibrary } from "./InterfaceLibrary";
+
+// Librería para hacer los gráficos
 import * as THREE from 'three';
+
+// El diagrama sobre una posición fija
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 export class Voronoi implements InterfaceLibrary{
-    menu: string =  '<div class="form-check mt-4">' +
-                        '<input class="form-check-input" type="checkbox" value="" id="azul-check">' +
-                        '<label class="form-check-label" for="azul-check">' +
-                          'Azul' +
-                        '</label>' +
-                    '</div>' +
-                    '<div class="form-check mt-4">' +
-                        '<input class="form-check-input" type="checkbox" value="" id="grises-check">' +
-                        '<label class="form-check-label" for="grises-check">' +
-                          'Grises' +
-                        '</label>' +
-                    '</div>' +
-                    '<div class="form-check mt-4">' +
-                        '<input class="form-check-input" type="checkbox" value="" id="auto-rotar-check">' +
-                        '<label class="form-check-label" for="auto-rotar-check">' +
-                          'Auto rotar' +
-                        '</label>' +
-                    '</div>' +
-                    '<div class="form-check mt-4">' +
-                      '<button type="submit" class="btn btn-primary btn-large">Aplicar</button>' +
-                    '</div>';
+    // El diagrama debería rotar automáticamente?
+    private isAutoRotating : boolean = false;
+
+    // Template del menú
+    menu: string = `
+        <div class="form-check mt-4">
+            <input class="form-check-input" type="checkbox" value="" id="azul-check">
+            <label class="form-check-label" for="azul-check">
+              Azul
+            </label>
+        </div>
+        <div class="form-check mt-4">
+            <input class="form-check-input" type="checkbox" value="" id="grises-check">
+            <label class="form-check-label" for="grises-check">
+              Grises
+            </label>
+        </div>
+        <div class="form-check mt-4">
+            <input class="form-check-input" type="checkbox" value="" id="auto-rotar-check">
+            <label class="form-check-label" for="auto-rotar-check">
+              Auto rotar
+            </label>
+        </div>
+        <div class="form-check mt-4">
+            <button type="submit" class="btn btn-primary btn-large">Aplicar</button>
+        </div>
+    `;
+
+    // Lista de elementos
     puntosRed: any[]= [];
+    // Lista de colores
     colors: any = {};
-    
+  
+    // Crear colores
+    // TODO: No sé qué hace esto
     setColor (checkbox:any, r:any, g:any, b:any) {
-      var coloraux, coloraux2, caux;
-      
-      if (checkbox.checked == true) {     
-          this.puntosRed.forEach( (punto) => {
-              coloraux = punto.material.color;
-              coloraux2 = coloraux.getHex();
-              caux = coloraux.r + coloraux.g + coloraux.b;
-              coloraux.r = r / caux;
-              coloraux.g = g / caux;
-              coloraux.b = b / caux;
-              punto.material.setValues({color: coloraux});
-              this.colors[coloraux.getHex()] = coloraux2;
-          });
-      } else {
+        // Variables auxiliares
+        var coloraux, coloraux2, caux;
         
-          this.puntosRed.forEach( (punto) => {
-              coloraux = punto.material.color;
-              caux = this.colors[coloraux.getHex()];              
-              punto.material.setValues({ color: caux });
-              
-          });
-          this.colors = {};
-      }
-    }
-
-    draw(json: any, c: any): void {
-      var puntos = json.p;
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-      
-      const renderer = new THREE.WebGLRenderer({ canvas: c });
-      const controls = new OrbitControls(camera, renderer.domElement);
-
-      camera.position.set(350,350,350);
-      controls.maxDistance = 500;
-      controls.maxDistance = 1000;
-      var group = new THREE.Group();
-      scene.add(group);
-    
-      // Especifica las figuras y su material
-      var colores: any;
-      var cs: any[] = [];
-      var mx = -10000, my = -10000, mz = -10000;
-      puntos.forEach(function (punto:any) {
-        console.log(punto)
-          var px = parseInt(punto.x);
-          var py = parseInt(punto.y);
-          var pz = parseInt(punto.z);
-          if (!colores.hasOwnProperty('' + punto.sb)) {
-              var p = new THREE.BoxGeometry();
-              colores['' + punto.sb] = p;
-  
-              cs.push(punto.sb);
-          }
-          var point = new THREE.Vector3();
-          point.x = px;
-          point.y = py;
-          point.z = pz;
-          if (px > mx) mx = px;
-          if (py > my) my = py;
-          if (pz > mz) mz = pz;
-  
-          colores['' + punto.sb].vertices.push(point);
-      });
-      cs.forEach( (color) => {
-          var aux = color * 111111;
-          var c = new THREE.PointsMaterial({ color: aux });
-          var puntomesh = new THREE.Points(colores['' + color], c);
-          group.add(puntomesh);
-          this.puntosRed.push(puntomesh);
-      });
-
-      function resizeCanvasToDisplaySize() {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-      
-        if (canvas.width !== width || canvas.height !== height) {
-          renderer.setSize(width, height, false);
-          camera.aspect = width / height;
-          camera.updateProjectionMatrix();
+        if (checkbox.checked == true) {     
+            this.puntosRed.forEach( (punto) => {
+                coloraux = punto.material.color;
+                coloraux2 = coloraux.getHex();
+                caux = coloraux.r + coloraux.g + coloraux.b;
+                coloraux.r = r / caux;
+                coloraux.g = g / caux;
+                coloraux.b = b / caux;
+                punto.material.setValues({color: coloraux});
+                this.colors[coloraux.getHex()] = coloraux2;
+            });
+        } else {
+            this.puntosRed.forEach( (punto) => {
+                coloraux = punto.material.color;
+                caux = this.colors[coloraux.getHex()];              
+                punto.material.setValues({ color: caux });
+                
+            });
+            this.colors = {};
         }
-      }
-  
-      function animate() {
-        requestAnimationFrame( animate );
-        resizeCanvasToDisplaySize()
-        controls.update();
-        renderer.render( scene, camera );
-      }
-
-      var centro = new THREE.Vector3();
-      centro.x = mx / 2;
-      centro.y = my / 2;
-      centro.z = mz / 2;
-      controls.target = centro;
-      animate();
     }
+
+    // Dibujar los puntos dados en el JSON
+    draw(json: any, c: any): void {
+        // Obtenemos los puntos del json
+        // p: {x: 0, y: 0, z: 0, sb: 502}
+        var puntos = json.p;
+
+        // Crear escena y cámara
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+        
+        // Crear el canvas
+        const renderer = new THREE.WebGLRenderer({ canvas: c });
+
+        // El diagrama se mueve respecto al centro del cubo
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.autoRotate = this.isAutoRotating;
+
+        // Posición inicial de la cámara
+        camera.position.set(350,350,350);
+
+        var posiciones: any = {};  // Lista de las posiciones de los puntos
+        var cs: any[] = [];  // Lista de los grupos dados por el campo p.sb
+        var mx = -10000, my = -10000, mz = -10000;  // Máximas coordenadas
+        
+        // Recorremos todos los puntos
+        puntos.forEach(function (punto:any) {
+            // Convertimos las coordenadas a enteros
+            var px = parseInt(punto.x);
+            var py = parseInt(punto.y);
+            var pz = parseInt(punto.z);
+
+            // Los puntos se agrupan con esta propiedad
+            var sb = '' + punto.sb
+
+            // Guardamos las sb para saber los grupos que tenemos
+            if (!cs.includes(sb)){
+                cs.push(sb);
+            }
+
+            // Agregamos la posición al diccionario de coordenadas con su grupo
+            if (!posiciones[sb]){
+                posiciones[sb] = new Array(px, py, pz);
+            }else{
+                posiciones[sb].push(px, py, pz);
+            }
+
+            // Actualizamos las coordenadas máximas
+            if (px > mx) mx = px;
+            if (py > my) my = py;
+            if (pz > mz) mz = pz;
+
+        });
+
+        // Esto debe cambiar debido al cambio a BufferGeometry
+        cs.forEach((csColor) => {
+            // Geometría que va contener todos los puntos con el mismo grupo
+            var geometry = new THREE.BufferGeometry();
+
+            // NOTE: LUIS: Por alguna razón el render seleccionado no acepta enteros
+            // así que los convierto en flotantes
+            var positions = Float32Array.from(posiciones[csColor]);
+
+            // Le mandamos al BufferGeometry las coordenadas de los puntos
+            // El objeto creado automáticamente convierte un Array en puntos de 3
+            // coordenadas
+            geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+
+            // Creamos el color del area
+            var pointColor = new THREE.Color( csColor * 111111);
+
+            // Creamos un material para los puntos
+            // TODO: El tamaño de los puntos lo puse a proposito para que se vea
+            // bien, tal vez sea necesario que sea especificado desde el JSON
+            var material = new THREE.PointsMaterial( { size: 2, color: pointColor } );
+
+            // NOTE: LUIS: Si algún día surge un problema puede ser por esta función
+            // geometry.computeBoundingSphere();
+            
+            // Creamos todos los puntos y los agregamos a la escena
+            var points = new THREE.Points( geometry, material );
+            scene.add(points);
+        });
+
+        function resizeCanvasToDisplaySize() {
+            const canvas = renderer.domElement;
+            const width = canvas.clientWidth;
+            const height = canvas.clientHeight;
+            
+            if (canvas.width !== width || canvas.height !== height) {
+                renderer.setSize(width, height, false);
+                camera.aspect = width / height;
+                camera.updateProjectionMatrix();
+            }
+        }
+
+        function animate() {
+            requestAnimationFrame( animate );
+            resizeCanvasToDisplaySize()
+            controls.update();
+            renderer.render( scene, camera );
+        }
+
+        // Calcular el centro del cubo para la rotación
+        var centro = new THREE.Vector3();
+        centro.x = mx / 2;
+        centro.y = my / 2;
+        centro.z = mz / 2;
+        controls.target = centro;
+
+        // Zoom máximo y mínimo
+        controls.minDistance = centro.length();
+        controls.maxDistance = 1000;
+
+        animate();
+    }
+
 }
